40d4fd0ea9af49a130907f06b9e0d73be14ee1e9
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-06-13 09:06:04 +0700

    membuat perulangan if dan else jika element yang dihapus berada di posisi terakhir array, kembali ke awal array

--- Cicularqueuses.cpp
+++ Cicularqueuses.cpp
@@ -51,5 +51,11 @@ class Queues{
             FRONT = -1;
             REAR = -1;
         }
+        else{
+            //jika element yang dihapus berada diposisi terakhir array, kembali ke awal array
+            if (FRONT == max -1)
+            FRONT=0;
+            else FRONT = FRONT +1;
+        }
     }
 };
