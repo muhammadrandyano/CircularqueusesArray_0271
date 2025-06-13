d639e2ba205f846244515fa65b11910062ee18c3
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-06-13 09:34:43 +0700

    membuat perulangan else jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir REAR

--- Cicularqueuses.cpp
+++ Cicularqueuses.cpp
@@ -79,5 +79,12 @@ class Queues{
             }
             cout << endl;
         }
+        else {
+            // jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir REAR
+            while (FRONT_position <= max -1){
+                cout << queue_array[FRONT_position]<< " ";
+                FRONT_position++;
+            }
+        }
     }
 };
