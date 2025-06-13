81671282eb66f106e1884519ab1cba3b519aac9d
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-06-13 09:35:57 +0700

    membuat perulangan while jika Iterasi dari awal array hingga REAR

--- Cicularqueuses.cpp
+++ Cicularqueuses.cpp
@@ -85,6 +85,14 @@ class Queues{
                 cout << queue_array[FRONT_position]<< " ";
                 FRONT_position++;
             }
-        }
+             FRONT_position = 0;
+
+             //Iterasi dari awal array hingga REAR
+            while(FRONT_position <= REAR_position){
+                cout << queue_array[FRONT_position]<< " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        } 
     }
 };
