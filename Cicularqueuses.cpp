e3cf29a668c854b353748fe489eef8dff58c178d
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-06-13 09:04:13 +0700

    cek jika antrian hanya memiliki satu elemen

--- Cicularqueuses.cpp
+++ Cicularqueuses.cpp
@@ -44,5 +44,12 @@ class Queues{
             cout << "Queue underflow\n";
             return;
         }
+        cout << "\nThe element deleted from the queue is: "<< queue_array[FRONT]<< endl;
+
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR){
+            FRONT = -1;
+            REAR = -1;
+        }
     }
 };
